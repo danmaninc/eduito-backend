20f9f609b21d36dd2c7c0be5b99ce51d4cc2ab14
Dan <dan@dminc.ru>
2024-06-03 22:54:11 +0300

    feat: init commit

---
+++ vars.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Vars():
+    DB_USER = os.environ.get('DB_USER')
+    DB_PASS = os.environ.get('DB_PASS')
+    DB_HOST = os.environ.get('DB_HOST')
+    DB_PORT = os.environ.get('DB_PORT')
+    DB_NAME = os.environ.get('DB_NAME')
+
+vars = Vars()
