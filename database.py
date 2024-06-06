2b8c7124254674ca1a49ad84ad8c18085c317b50
Dan <dan@dminc.ru>
2024-06-06 19:46:52 +0300

    fix(env): error when loading

--- database.py
+++ database.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from vars import vars
+from vars import env_vars
 
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{vars.DB_USER}:{vars.DB_PASS}@{vars.DB_HOST}:{vars.DB_PORT}/{vars.DB_NAME}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{env_vars.DB_USER}:{env_vars.DB_PASS}@{env_vars.DB_HOST}:{env_vars.DB_PORT}/{env_vars.DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- vars.py
+++ vars.py
@@ -1,5 +1,11 @@
 import os
 
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
 
 class Vars():
     DB_USER = os.environ.get('DB_USER')
@@ -8,4 +14,5 @@ class Vars():
     DB_PORT = os.environ.get('DB_PORT')
     DB_NAME = os.environ.get('DB_NAME')
 
-vars = Vars()
+
+env_vars = Vars()
