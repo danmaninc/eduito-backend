24e5ed031aaedb7620dc7963409c8798f8087c97
Dan <dan@dminc.ru>
2024-06-06 19:47:21 +0300

    feat: basic functionality

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, Depends, HTTPException
+import crud
+import models
+import schemas
+from database import SessionLocal, engine
+from sqlalchemy.orm import Session
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.post("/users", response_model=schemas.UserCreateResponse)
+def read_item(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db, user=schemas.UserCreate(
+        username=user.username,
+        email=user.email,
+        password=user.password)
+                            )
