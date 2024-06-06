b3dc7701a53586a12616978fcbafdfc65bb886ac
Dan <dan@dminc.ru>
2024-06-06 19:46:13 +0300

    chore: user retrieval by email

--- crud.py
+++ crud.py
@@ -1,10 +1,17 @@
 import hashlib
 from sqlalchemy.orm import Session
 
-from . import models, schemas
+import models
+import schemas
+
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
 
 def create_user(db: Session, user: schemas.UserCreate):
     hashed = hashlib.sha256(user.password.encode("utf-8")).hexdigest()
