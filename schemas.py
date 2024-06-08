1dc45270df416c5c6e3614951d54e5c839088543
Dan <dan@dminc.ru>
2024-06-08 22:37:29 +0300

    feat(schemas): UserGetResponse

--- schemas.py
+++ schemas.py
@@ -1,16 +1,26 @@
 from pydantic import BaseModel
 
+
 class UserBase(BaseModel):
     username: str
     email: str
 
+
 class UserCreate(UserBase):
     password: str
+
 
 class UserCreateResponse(UserBase):
     id: int
     is_verified: bool
 
+
 class User(UserBase):
     id: int
+    is_verified: bool
+
+
+class UserGetResponse(BaseModel):
+    id: int
+    username: str
     is_verified: bool
